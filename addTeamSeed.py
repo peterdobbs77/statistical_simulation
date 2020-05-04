6cd26186de76518eea461f5242f97d8655d63458
Peter Dobbs <dobbsp8@gmail.com>
2020-05-04 13:17:01 -0500

    Update addTeamSeed.py

--- addTeamSeed.py
+++ addTeamSeed.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 filepath = './data/ultimate/'
 extpath = 'collegenationals_men/'
-filename = 'all_poolplay.csv'
+filename = 'all_reorganized.csv'
 
 df = pd.read_csv(filepath+extpath+filename)
-df["home_seed"] = df["home_team"].str.extract(r'(\d+)')
-df["away_seed"] = df["away_team"].str.extract(r'(\d+)')
+df["winner_seed"] = df["winner_team"].str.extract(r'(\d+)')
+df["loser_seed"] = df["loser_team"].str.extract(r'(\d+)')
 df.to_csv(filepath+extpath+filename, index=False)
